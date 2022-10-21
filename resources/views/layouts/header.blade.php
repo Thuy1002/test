7e1c44cc8f26cd2c8273ccdecb98c48100a84156
thuy1002 <thuy1002dangthanh@gmail.com>
2022-10-21 11:35:34 +0700

    logout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1666,13 +1666,15 @@
                 <div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2"
                     id="kt_quick_user_toggle">
                     <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">{{$objUser->name }}</span>
-                    <span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
-                        <span class="symbol-label font-size-h5 font-weight-bold"></span>
-                    </span>
-                
+                    <span
+                        class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">{{ $objUser->name }}</span>
                 </div>
-               
+                <a class="btn" href="{{ route('logout') }}">
+                    <span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
+                        <span class="symbol-label font-size-h5 font-weight-bold">Out</span>
+                    </span>
+                </a>
+
             </div>
             <!--end::User-->
         </div>
